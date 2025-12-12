15b7f4a6bebcaad080408338a18bf1ca70decd7f
techmaster-backend <contact.us.in.edu@gmail.com>
2025-12-12 16:34:40 +0530

    Add build 486177a2-1afa-4fbd-a380-8db0637589f7 file capacitor.config.ts

---
+++ builds/486177a2-1afa-4fbd-a380-8db0637589f7/capacitor.config.ts
@@ -0,0 +1,14 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  "appId": "com.karbonsites.minigame",
+  "appName": "Mini Game",
+  "webDir": "www",
+  "server": {
+    "androidScheme": "https"
+  }
+};
+
+export default config;
+    
